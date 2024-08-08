735203d3cf15f5e9fb523bf8808ef91e0b2e5d9b
divino-souza <divino.souza@tecnospeed.com.br>
2024-08-08 10:08:15 -0300

    -a

---
+++ dist/js/src/app.js
@@ -0,0 +1,14 @@
+import { Negociacao } from "./models/negociacao.js";
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+const negociacao = new Negociacao(new Date(), 10, 100);
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+if (form) {
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        controller.adiciona();
+    });
+}
+else {
+    throw Error('Não foi possivel inicializar a aplicação. Verifique se o form existe');
+}
---
+++ dist/js/src/controllers/negociacao-controller.js
@@ -0,0 +1,43 @@
+import { DiasDaSemana } from "../enums/dias-da-semana.js";
+import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
+import { MensageView } from "../../views/mensagem-view.js";
+import { NegociacoesView } from "../../views/negociacoes-views.js";
+export class NegociacaoController {
+    constructor() {
+        this.negociacoes = new Negociacoes;
+        this.negociacoesView = new NegociacoesView('#negociacoesView', true);
+        this.mensagemView = new MensageView('#mensagemView');
+        this.SABADO = 6;
+        this.DOMINGO = 0;
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector("#quantidade");
+        this.inputValor = document.querySelector('#valor');
+        this.negociacoesView.update(this.negociacoes);
+    }
+    adiciona() {
+        const negociacao = Negociacao.criaDe(this.inputData.value, this.inputQuantidade.value, this.inputValor.value);
+        if (!this.ehDIaUtil(negociacao.data)) {
+            this.mensagemView
+                .update('Apenas negociações em dias uteis são aceitas');
+            return;
+        }
+        this.negociacoes.adiciona(negociacao);
+        this.limparFormulario();
+        this.atualizaView();
+    }
+    ehDIaUtil(data) {
+        return data.getDay() > DiasDaSemana.DOMINGO
+            && data.getDay() < DiasDaSemana.SABADO;
+    }
+    limparFormulario() {
+        this.inputData.value = "";
+        this.inputQuantidade.value = "";
+        this.inputValor.value = "";
+        this.inputData.focus();
+    }
+    atualizaView() {
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update("Negociação adicionada com sucesso");
+    }
+}
